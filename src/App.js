3d659497f3c00c3158dd2a6d0d6cd17f5c8e3ac5
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-01-14 18:21:08 +0100

    react quote machine add svg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import QuoteBox from "./components/QuoteBox";
 import "./App.css";
 import './assets/css/fonts.css';
+import quoteiconleft from './quote-a-left.svg';
+import quoteiconright from './quote-a-right.svg';
 
 export default function App() {
   /* ici la logique pour demander une nouvelle citation */
   return (
     <div className="App">
+      <h1 className="page-title">
+      <img className="quote-icon-left" src={quoteiconleft} alt="some file"  height='30'
+      width='30'/>React Quote Machine<img className="quote-icon-right" src={quoteiconright} alt="some file"  height='30'
+      width='30'/></h1>
       <QuoteBox />
     </div>
   );
